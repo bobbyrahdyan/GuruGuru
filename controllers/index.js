c883b08e37597314d62ebae16fd440c585d34148
Bobby Rahdyan <bobby.notokoesoemo@gmail.com>
2022-11-10 17:34:38 +0700

    1

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,10 @@
 const {User, Course, CourseDetail, UserCourse} = require('../models')
 
 class Controller{
-    static students(req, res){
-        User.findAll({where: {
-            role: 'student'
-        }, inlcude: Course})
+    static students(req, res) {
+        User.findAll({
+            include: Course
+        })
         .then(students => {
             res.send(students)
         })
